d294540232a57ec071b3d3f91e53ed5f5128b281
torculus <20175597+torculus@users.noreply.github.com>
2025-05-23 14:50:33 -0400

    rustfmt, update scene logic, rename selected -> scene

--- src/main.rs
+++ src/main.rs
@@ -18,27 +18,33 @@
  *
  */
 extern crate getopts;
+use chrono::Date;
+use chrono::DateTime;
 use getopts::Options;
 
 extern crate crossterm;
-use crossterm::{cursor, queue, QueueableCommand, style, terminal};
 use crate::crossterm::style::Stylize;
-use crossterm::style::Color;
-use crossterm::cursor::MoveTo;
-use crossterm::cursor::Hide;
-use crossterm::terminal::EnterAlternateScreen;
-use crossterm::terminal::Clear;
-use crossterm::execute;
-use crossterm::style::PrintStyledContent;
-use std::io::stdout;
-use crossterm::terminal::size;
 use crate::crossterm::ExecutableCommand;
+use crossterm::cursor::Hide;
+use crossterm::cursor::MoveTo;
+use crossterm::execute;
+use crossterm::style::Color;
+use crossterm::style::PrintStyledContent;
+use crossterm::terminal::size;
+use crossterm::terminal::Clear;
+use crossterm::terminal::EnterAlternateScreen;
+use crossterm::{cursor, queue, style, terminal, QueueableCommand};
+use std::io::stdout;
 
 use chrono::{Datelike, Local};
 use rand::Rng;
 use std::{env, io, thread, time};
 
-const brown: Color = Color::Rgb { r: 139, g: 69, b: 19 };
+const BROWN: Color = Color::Rgb {
+    r: 139,
+    g: 69,
+    b: 19,
+};
 
 fn main() {
     let mut stdout = stdout();
@@ -84,7 +90,7 @@ fn main() {
     let five_min = time::Duration::from_secs(300);
 
     let mut dt: chrono::DateTime<Local>;
-    let mut selected: u32;
+    let mut scene: u32;
 
     loop {
         //clear the screen on each iteration
@@ -92,34 +98,38 @@ fn main() {
 
         dt = Local::now();
 
-        if dt.month() == noel_month {
-            match dt.day() {
-                //before Christmas Eve
-                1..23 => selected = 1,
-                //Christmas Eve to 12 days of Christmas
-                24..25 => selected = 2,
-                //Epiphany
-                _ => selected = 3,
-            }
-        } else {
-            selected = 0;
+        let christmas = format!("Dec 25, {} 00:00:00 +0000", dt.year());
+        let christmas = christmas.as_str();
+        let christmas = DateTime::parse_from_str(christmas, "%b %d, %Y %H:%M:%S %z").unwrap();
+
+        let days_till_christmas = i64::from(dt.ordinal()) - i64::from(christmas.ordinal());
+
+        match days_till_christmas {
+            //Christmas season .. Christmas Adam
+            -24..-1 => scene = 1,
+            //Christmas Eve to 12 days of Christmas
+            -1..13 => scene = 2,
+            //Epiphany to Candlemas
+            13..41 => scene = 3,
+            //Out of season
+            _ => scene = 0,
         }
 
-        print_scene(selected);
+        print_scene(scene);
         thread::sleep(five_min);
     }
 }
 
-fn print_scene(selected: u32) {
+fn print_scene(scene: u32) {
     let size = size();
     let (term_w, term_h) = match size {
         Ok(size) => size,
-	Err(error) => panic!("{}", error.to_string()),
+        Err(error) => panic!("{}", error.to_string()),
     };
 
     print_stable_manger(term_w, term_h);
 
-    match selected {
+    match scene {
         0 => {}
         1 => {
             print_mary_joseph(term_w, term_h);
@@ -139,9 +149,9 @@ fn print_scene(selected: u32) {
     loop {
         //twinkle stars in sky every second
         print_sky(term_w);
-        print_star(selected, term_w);
+        print_star(scene, term_w);
 
-        if selected > 1 {
+        if scene > 1 {
             print_rays(term_w, round);
             round = (round + 1) % 4;
         }
@@ -152,38 +162,42 @@ fn print_scene(selected: u32) {
 
 fn print_stable_manger(width: u16, height: u16) {
     let mut stdout = stdout();
-    queue!(stdout,
-    	MoveTo(width / 2 - 2, height - 11),
-	PrintStyledContent("./^\\.".with(brown))
-	);
+    queue!(
+        stdout,
+        MoveTo(width / 2 - 2, height - 11),
+        PrintStyledContent("./^\\.".with(BROWN))
+    );
 
     for i in 1..5 {
         //stable roof
-	queue!(stdout,
-	    MoveTo(width / 2 - 2 - 3 * i, height - 11 + i),
-	    PrintStyledContent(".%%.".with(brown)),
-	    MoveTo(width / 2 - 2 + 3 * i, height - 11 + i),
-	    PrintStyledContent(".%%.".with(brown))
-	    );
+        queue!(
+            stdout,
+            MoveTo(width / 2 - 2 - 3 * i, height - 11 + i),
+            PrintStyledContent(".%%.".with(BROWN)),
+            MoveTo(width / 2 - 1 + 3 * i, height - 11 + i),
+            PrintStyledContent(".%%.".with(BROWN))
+        );
     }
 
     for j in 1..7 {
         //stable walls
-	queue!(stdout,
-	    MoveTo(width / 2 - 13, height - 7 + j),
-	    PrintStyledContent("##".with(brown)),
-	    MoveTo(width / 2 + 12, height - 7 + j),
-	    PrintStyledContent("##".with(brown))
-	    );
+        queue!(
+            stdout,
+            MoveTo(width / 2 - 13, height - 7 + j),
+            PrintStyledContent("##".with(BROWN)),
+            MoveTo(width / 2 + 12, height - 7 + j),
+            PrintStyledContent("##".with(BROWN))
+        );
     }
 
     //print manger
-    queue!(stdout,
-	MoveTo(width / 2 - 3, height - 2),
-	PrintStyledContent(r#"""""""#.yellow()),
-	MoveTo(width / 2 - 3, height - 1),
-	PrintStyledContent(r#"/ \ / \"#.with(brown))
-	);
+    queue!(
+        stdout,
+        MoveTo(width / 2 - 2, height - 2),
+        PrintStyledContent(r#"""""""#.yellow()),
+        MoveTo(width / 2 - 3, height - 1),
+        PrintStyledContent(r#"/ \ / \"#.with(BROWN))
+    );
 }
 
 fn print_sky(width: u16) {
@@ -191,10 +205,11 @@ fn print_sky(width: u16) {
     let mut stdout = stdout();
 
     //clear the sky
-    execute!(stdout,
-	MoveTo(width, 4),
-	Clear(terminal::ClearType::FromCursorUp)
-	);
+    execute!(
+        stdout,
+        MoveTo(width, 4),
+        Clear(terminal::ClearType::FromCursorUp)
+    );
 
     for _i in 1..10 {
         //generating (0,0) will throw an error: Goto is one-based
@@ -202,45 +217,45 @@ fn print_sky(width: u16) {
         let y = rng.gen_range(1..4);
 
         //set a star at (x,y)
-	queue!(stdout,
-	    MoveTo(x, y),
-	    PrintStyledContent("*".white())
-	    );
+        queue!(stdout, MoveTo(x, y), PrintStyledContent("*".white()));
     }
 }
 
-fn print_star(selected: u32, width: u16) {
+fn print_star(scene: u32, width: u16) {
     let mut stdout = stdout();
-    match selected {
+    match scene {
         0 => {
-	    queue!(stdout,
-		MoveTo(width / 2 + 2, 1),
-		PrintStyledContent("*".yellow()),
-		MoveTo(width / 2, 2),
-		PrintStyledContent("*".yellow()),
-		MoveTo(width / 2 - 2, 3),
-		PrintStyledContent("*".yellow())
-		);
+            queue!(
+                stdout,
+                MoveTo(width / 2 + 2, 1),
+                PrintStyledContent("*".yellow()),
+                MoveTo(width / 2, 2),
+                PrintStyledContent("*".yellow()),
+                MoveTo(width / 2 - 2, 3),
+                PrintStyledContent("*".yellow())
+            );
         }
         1 => {
-	    queue!(stdout,
-		MoveTo(width / 2, 1),
-		PrintStyledContent(".".yellow()),
-		MoveTo(width / 2 - 2, 2),
-		PrintStyledContent(". * .".yellow()),
-		MoveTo(width / 2, 3),
-		PrintStyledContent(".".yellow())
-		);
+            queue!(
+                stdout,
+                MoveTo(width / 2, 1),
+                PrintStyledContent(".".yellow()),
+                MoveTo(width / 2 - 2, 2),
+                PrintStyledContent(". * .".yellow()),
+                MoveTo(width / 2, 3),
+                PrintStyledContent(".".yellow())
+            );
         }
         _ => {
-	    queue!(stdout,
-		MoveTo(width / 2, 1),
-		PrintStyledContent(":".yellow()),
-		MoveTo(width / 2 - 3, 2),
-		PrintStyledContent(".. * ..".yellow()),
-		MoveTo(width / 2, 3),
-		PrintStyledContent(":".yellow())
-		);
+            queue!(
+                stdout,
+                MoveTo(width / 2, 1),
+                PrintStyledContent(":".yellow()),
+                MoveTo(width / 2 - 3, 2),
+                PrintStyledContent(".. * ..".yellow()),
+                MoveTo(width / 2, 3),
+                PrintStyledContent(":".yellow())
+            );
         }
     }
 }
@@ -249,78 +264,89 @@ fn print_mary_joseph(width: u16, height: u16) {
     let mut stdout = stdout();
 
     //print Mary
-    queue!(stdout,
-	MoveTo(width / 2 - 6, height - 4),
-	PrintStyledContent(".@".blue()),
-	MoveTo(width / 2 - 6, height - 3),
-	PrintStyledContent("%%#".blue()),
-	MoveTo(width / 2 - 6, height - 2),
-	PrintStyledContent("%%".blue()),
-	MoveTo(width / 2 - 7, height - 1),
-	PrintStyledContent("%%%".blue())
-	);
+    queue!(
+        stdout,
+        MoveTo(width / 2 - 6, height - 4),
+        PrintStyledContent(".@".blue()),
+        MoveTo(width / 2 - 6, height - 3),
+        PrintStyledContent("%%#".blue()),
+        MoveTo(width / 2 - 6, height - 2),
+        PrintStyledContent("%%".blue()),
+        MoveTo(width / 2 - 7, height - 1),
+        PrintStyledContent("%%%".blue())
+    );
 
-    queue!(stdout,
-	// Joseph's staff
-	MoveTo(width / 2 + 5, height - 5),
-	PrintStyledContent("?".with(brown)),
-	MoveTo(width / 2 + 5, height - 4),
-	PrintStyledContent("|".with(brown)),
-	MoveTo(width / 2 + 5, height - 3),
-	PrintStyledContent("|".with(brown)),
-	MoveTo(width / 2 + 5, height - 2),
-	PrintStyledContent("|".with(brown)),
-	MoveTo(width / 2 + 5, height - 1),
-	PrintStyledContent("|".with(brown)),
-	// Joseph's body
-	MoveTo(width / 2 + 7, height - 5),
-	PrintStyledContent("@".green()),
-	MoveTo(width / 2 + 6, height - 4),
-	PrintStyledContent("#%".green()),
-	MoveTo(width / 2 + 7, height - 3),
-	PrintStyledContent("%%%".green()),
-	MoveTo(width / 2 + 7, height - 2),
-	PrintStyledContent("%%%".green()),
-	MoveTo(width / 2 + 8, height - 1),
-	PrintStyledContent("%%%".green())
-	);
+    queue!(
+        stdout,
+        // Joseph's staff
+        MoveTo(width / 2 + 5, height - 5),
+        PrintStyledContent("?".with(BROWN)),
+        MoveTo(width / 2 + 5, height - 4),
+        PrintStyledContent("|".with(BROWN)),
+        MoveTo(width / 2 + 5, height - 3),
+        PrintStyledContent("|".with(BROWN)),
+        MoveTo(width / 2 + 5, height - 2),
+        PrintStyledContent("|".with(BROWN)),
+        MoveTo(width / 2 + 5, height - 1),
+        PrintStyledContent("|".with(BROWN)),
+        // Joseph's body
+        MoveTo(width / 2 + 7, height - 5),
+        PrintStyledContent("@".green()),
+        MoveTo(width / 2 + 6, height - 4),
+        PrintStyledContent("#%".green()),
+        MoveTo(width / 2 + 7, height - 3),
+        PrintStyledContent("%%%".green()),
+        MoveTo(width / 2 + 7, height - 2),
+        PrintStyledContent("%%%".green()),
+        MoveTo(width / 2 + 8, height - 1),
+        PrintStyledContent("%%%".green())
+    );
 }
 
 fn print_jesus(width: u16, height: u16) {
     let mut stdout = stdout();
 
-    queue!(stdout,
-	MoveTo(width / 2 - 2, height - 3),
-	PrintStyledContent("@##".white())
-	);
+    queue!(
+        stdout,
+        MoveTo(width / 2 - 2, height - 3),
+        PrintStyledContent("@##".white())
+    );
 }
 
 fn print_magi(width: u16, height: u16) {
-    let purple = Color::Rgb { r: 159, g: 43, b: 154 };
-    let orange = Color::Rgb { r: 255, g: 87, b: 51 };
+    let purple = Color::Rgb {
+        r: 159,
+        g: 43,
+        b: 154,
+    };
+    let orange = Color::Rgb {
+        r: 255,
+        g: 87,
+        b: 51,
+    };
     let mut stdout = stdout();
 
     //print Magi
     /*println!(
-        "{goto}{yellow}_     _     _  {brown}@   %{reset}",
+        "{goto}{yellow}_     _     _  {BROWN}@   %{reset}",
         goto = cursor::Goto(width / 2 + 18, height - 6)
     );
     println!(
-        "{goto}{red}@     {purple}@     {orange}@   {brown}#%%%%%%{reset}",
+        "{goto}{red}@     {purple}@     {orange}@   {BROWN}#%%%%%%{reset}",
         goto = cursor::Goto(width / 2 + 18, height - 5)
     );
-    println!("{goto}{yellow}# {red}%%\\ {green}^ {purple}%%\\ {purple}o {orange}%%\\ {brown}%%%%%%%%{reset}",
-    	goto = cursor::Goto(width/2+16, height-4));
+    println!("{goto}{yellow}# {red}%%\\ {green}^ {purple}%%\\ {purple}o {orange}%%\\ {BROWN}%%%%%%%%{reset}",
+        goto = cursor::Goto(width/2+16, height-4));
     println!(
-        "{goto}{yellow}#{red}#%%% {green}#{purple}#%%% {purple}#{orange}#%%%  {brown}#%%%%={reset}",
+        "{goto}{yellow}#{red}#%%% {green}#{purple}#%%% {purple}#{orange}#%%%  {BROWN}#%%%%={reset}",
         goto = cursor::Goto(width / 2 + 16, height - 3)
     );
     println!(
-        "{goto}{red}%%%   {purple}%%%   {orange}%%%  {brown}=    ={reset}",
+        "{goto}{red}%%%   {purple}%%%   {orange}%%%  {BROWN}=    ={reset}",
         goto = cursor::Goto(width / 2 + 18, height - 2)
     );
     println!(
-        "{goto}{red}%%%   {purple}%%%   {orange}%%% {brown}.=   .={reset}",
+        "{goto}{red}%%%   {purple}%%%   {orange}%%% {BROWN}.=   .={reset}",
         goto = cursor::Goto(width / 2 + 18, height - 1)
     );*/
 }
@@ -328,21 +354,19 @@ fn print_magi(width: u16, height: u16) {
 fn print_rays(width: u16, round: u16) {
     let mut stdout = stdout();
 
-    queue!(stdout,
-	MoveTo(width / 2 - 1 - round, 4 + round),
-	PrintStyledContent("/".yellow()),
-	MoveTo(width / 2, 4 + round),
-	PrintStyledContent("|".yellow()),
-	MoveTo(width / 2 + 1 + round, 4 + round),
-	PrintStyledContent("\\".yellow())
-	);
+    queue!(
+        stdout,
+        MoveTo(width / 2 - 1 - round, 4 + round),
+        PrintStyledContent("/".yellow()),
+        MoveTo(width / 2, 4 + round),
+        PrintStyledContent("|".yellow()),
+        MoveTo(width / 2 + 1 + round, 4 + round),
+        PrintStyledContent("\\".yellow())
+    );
 
     if round == 0 {
         //clear the rays
-	queue!(stdout,
-	    MoveTo(width, 7),
-	    PrintStyledContent("\\".yellow())
-	    );
+        queue!(stdout, MoveTo(width, 7), PrintStyledContent("\\".yellow()));
     }
 }
 
