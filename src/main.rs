b6faf5a4ea5ad0f9be36acda2fef0a47eddbabb1
torculus <20175597+torculus@users.noreply.github.com>
2025-07-18 08:47:19 -0400

    prep for submission to crates.io

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
-/* naTERMity - an animated nativity terminal screensaver
- * Copyright (C) 2024-2025 Benjamin S Osenbach
- *
- * Inspired by ChristBASHTree (https://github.com/sergiolepore/ChristBASHTree)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
+// naTERMity - an animated nativity terminal screensaver
+// Copyright (C) 2024-2025 Benjamin S Osenbach
+//
+// Inspired by ChristBASHTree (https://github.com/sergiolepore/ChristBASHTree)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 extern crate getopts;
 use getopts::Options;
 
